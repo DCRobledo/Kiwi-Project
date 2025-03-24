a11c0688b21e83c7d0dcd73c2c43d36f84a70043
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-03-24 12:28:49 +0100

    Set up frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+﻿import { useDispatch, useSelector } from 'react-redux';
+import { increment } from './features/counter/counterSlice';
+import { RootState } from './app/store';
+
+function App() {
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="p-6">
+            <p className="text-xl">Count: {count}</p>
+            <button
+                onClick={() => dispatch(increment())}
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+            >
+                Increment
+            </button>
+        </div>
+    );
+}
+
+export default App;
